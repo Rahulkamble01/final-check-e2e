b7b5e0d11b1fade929f09dc5d090c079d1d51a56
Kiran Ravariya <kiran.ravariya@cognizant.com>
2019-02-02 20:41:03 +0530

    Protractor Testing for login page added

---
+++ angular/skynews/e2e/src/pages/signup/signup.po.ts
@@ -0,0 +1,40 @@
+import { browser, by, element, promise, ElementFinder, ElementArrayFinder } from 'protractor';
+
+export class SignUpPage {
+
+    navigateToLoginPage() {
+        return browser.get('/');
+    }
+
+    getSignUpButton() {
+        return element(by.className('btn btn-primary signup-btn'));
+    }
+
+    getSubmitButton(){
+        return element(by.className('btn btn-success btn-block'));
+    }
+    sendNameForSignUp() {
+        return element(by.id('loginemail'));
+    }
+
+    sendEmailForSignUp() {
+        return element(by.id('loginpassword'));
+    }
+
+    sendPasswordForSignUp() {
+        return element(by.id('loginpassword'));
+    }
+
+    sendLanguageForSignUp() {
+        return element(by.id('loginemail'));
+    }
+
+    getLoginButton() {
+        return element(by.className('btn btn-primary btn-block signup-btn float-right'));
+    }
+
+    getErrorMessage() {
+        return element(by.className('col form-group alert alert-danger'));
+    }
+
+}
